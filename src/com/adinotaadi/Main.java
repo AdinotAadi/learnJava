9386aa296efd60d46ce2e387c8ec4c5df8a16b79
Aditya Patel <patel.aditya1@outlook.com>
2022-03-18 12:53:09 +0530

    Initia commit.

---
+++ src/com/adinotaadi/Main.java
@@ -0,0 +1,8 @@
+package com.adinotaadi; // This is the name of the package.
+
+public class Main { // The name of the class should be the same as the name of the file.
+
+    public static void main(String[] args) {    // This is the main function. The executions of the program starts from here.
+        System.out.println("Hello, World!");    // This prints "Hello, World!" on the screen.
+    }
+}
