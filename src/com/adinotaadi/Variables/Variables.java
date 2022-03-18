07191e01a24b2378dd20a00fa07e9f89e0646e01
Aditya Patel <patel.aditya1@outlook.com>
2022-03-18 13:21:28 +0530

    Third commit.

---
+++ src/com/adinotaadi/Variables/Variables.java
@@ -0,0 +1,15 @@
+package com.adinotaadi.Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        // Following are the most common data types in Java.
+
+        int age = 19; // 4 bytes long.
+        float pi = 3.14f; // 4 bytes long.
+        char gender = 'M'; // 2 bytes long.
+        double longPi = 3.14956099; // 8 bytes long.
+        long phnNumber = 7977990620L; // 8 bytes long.
+        boolean isAdult = true; // 1 bit long.
+        String name = "Aditya"; // Depends on the length of the string.
+    }
+}
